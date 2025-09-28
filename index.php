7e781c265d1d3c50f917bac8fe96c929abb1672f
charlesnavalta <charlesdarwinnavalta@gmail.com>
2025-09-28 12:12:50 +0800

    update index.php

--- index.php
+++ index.php
@@ -1,5 +1,19 @@
 <?php
     echo "Andrea Pauline L. Monis";
-
+    echo "<br>";
     echo "Charles Navalta";
+
 ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Library System</title>
+</head>
+<body>
+    <h1>Welcome to the Library System</h1>
+    <nav>
+        <a href="try.html">Try</a>
+    </nav>
+</body>
+</html>

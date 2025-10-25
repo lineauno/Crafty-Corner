0ecdfdb47b76c2ef0c19c1a76b87c606102ae3df
lineauno <164720569+lineauno@users.noreply.github.com>
2025-10-25 23:44:42 +0800

    Added the final draft code of the running application

--- src/index.js
+++ src/index.js
@@ -2,9 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const rootElement = document.getElementById('root');
+
+if (rootElement) {
+    ReactDOM.createRoot(rootElement).render(
+        <React.StrictMode>
+            <App />
+        </React.StrictMode>
+    );
+} else {
+    console.error("Failed to find the root element in the DOM.");
+}
